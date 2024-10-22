29a2854cacd836a3da746e3f7c3e2bcda67e135d
fai-sas <craftedbyfaisal@gmail.com>
2024-10-22 20:17:28 +0600

    insert data into students, courses and enrollment table

--- postgres-basics.sql
+++ postgres-basics.sql
@@ -24,3 +24,86 @@ CREATE TABLE enrollment (
     student_id INT REFERENCES students (student_id),
     course_id INT REFERENCES courses (course_id)
 );
+
+-- Insert data into the students table:
+INSERT INTO
+    students (
+        student_name,
+        age,
+        email,
+        frontend_mark,
+        backend_mark,
+        status
+    )
+VALUES (
+        'Sameer',
+        21,
+        'sameer@example.com',
+        48,
+        60,
+        NULL
+    ),
+    (
+        'Zoya',
+        23,
+        'zoya@example.com',
+        52,
+        58,
+        NULL
+    ),
+    (
+        'Nabil',
+        22,
+        'nabil@example.com',
+        37,
+        46,
+        NULL
+    ),
+    (
+        'Rafi',
+        24,
+        'rafi@example.com',
+        41,
+        40,
+        NULL
+    ),
+    (
+        'Sophia',
+        22,
+        'sophia@example.com',
+        50,
+        52,
+        NULL
+    ),
+    (
+        'Hasan',
+        23,
+        'hasan@gmail.com',
+        43,
+        39,
+        NULL
+    );
+
+-- Insert data into the courses table:
+INSERT INTO
+    courses (
+        course_id,
+        course_name,
+        credits
+    )
+VALUES (1, 'Next.js', 3),
+    (2, 'React.js', 4),
+    (3, 'Databases', 3),
+    (4, 'Prisma', 3);
+
+-- Insert data into the enrollment table:
+INSERT INTO
+    enrollment (
+        enrollment_id,
+        student_id,
+        course_id
+    )
+VALUES (1, 1, 1),
+    (2, 1, 2),
+    (3, 2, 1),
+    (4, 3, 2);
