831f953abd889a8e7766bbb40785fc2788d586dd
fai-sas <craftedbyfaisal@gmail.com>
2024-10-22 20:32:14 +0600

    Query 8: Retrieve the names of students whose email addresses contain 'example.com'

--- postgres-basics.sql
+++ postgres-basics.sql
@@ -166,3 +166,6 @@ GROUP BY
 
 -- Query 7: Calculate and display the average age of all students
 SELECT ROUND(AVG(age), 2) AS average_age FROM students;
+
+-- Query 8: Retrieve the names of students whose email addresses contain 'example.com'
+SELECT student_name FROM students WHERE email LIKE '%example.com%';
